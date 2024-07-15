a051c489acf1e8be03d7b498cd99cb3da6662e1d
sameer <maharjansameer111@gmail.com>
2024-07-15 22:17:47 +0545

    FE:locomotive js start

---
+++ locomotive js/script.js
@@ -0,0 +1,6 @@
+//locomotive
+const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container'), //parent dive , parent div should ot contain width and height
+  smooth: true,
+  lerp:0.1
+});
